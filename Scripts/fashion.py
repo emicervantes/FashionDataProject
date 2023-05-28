2159582a20b30686e27a02d03094862cabb1518f
emicervantes <emicer@hotmail.co.jp>
2023-05-27 18:34:31 -0700

    changed py

--- Scripts/fashion.py
+++ Scripts/fashion.py
@@ -8,6 +8,6 @@ from PIL import Image
 # Title and headers
 st.title("FITTED AND PREDICTED")
 st.write("**Developers:** Emi Cervantes, Christina Orengo, Nathan Samarasena")
-image = Image.open('choso.jpeg')
+image = Image.open('spongebob.png')
 
 
