b90a350149e6fd8313026ed991b573053a9c7a4c
emicervantes <emicer@hotmail.co.jp>
2023-05-27 19:44:30 -0700

    changing st

--- Scripts/fashion.py
+++ Scripts/fashion.py
@@ -23,8 +23,15 @@ col1, col2 = st.columns(2)
 with col1:
     st.header("spongebob says 'hello!'")
     factor = col1.selectbox("Choose Your Cloth Category:", category)
-    waist = st.slider('Your waist: ', 0, 130, 25)
-    height = st.slider('Your height: ', 0, 130, 25)
+    waist = st.slider('Your waistt: ' , 20, 50, 1)
+    height = st.select_slider(
+    'Your height: ',
+    options=["4'00ft", "4'01ft", "4'02ft", "4'03ft", "4'04ft", "4'05ft",
+    "4'06ft", "4'07ft", "4'08ft", "4'09ft", "4'10ft", "4'11ft", 
+    "5'00ft", "5'01ft", "5'01ft", "5'02ft", "5'03ft", "5'04ft", "5'05ft", "4'01ft",
+    "5'06ft", "5'07ft", "5'08ft", "5'09ft", "5'10ft", "5'11ft", "6'01ft", "6'02ft",
+    "6'03ft", "6'04ft", "6'05ft", "6'06ft", "6'07ft", "6'08ft", ,"6'09ft", "6'10ft",
+    "6'11ft"])
     cup_size = st.slider('Your cup size: ', 0, 130, 25)
     bra_size = st.slider('Your bra size: ', 0, 130, 25)
 with col2:
