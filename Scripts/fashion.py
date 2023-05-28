1bd8e52c041858c016c0a49f38319a6c484ab997
emicervantes <emicer@hotmail.co.jp>
2023-05-27 19:02:50 -0700

    changing st

--- Scripts/fashion.py
+++ Scripts/fashion.py
@@ -5,6 +5,16 @@ import numpy as np
 import altair as alt
 from PIL import Image
 
+# Data analysis
+df = pd.read_csv("../data/fashion3.csv")
+df.iloc[:,2:13]
+col_lst = list(range(2,13))
+col_lst.extend([15,17])
+df = df.iloc[:,col_lst]
+df_clean = df.dropna()
+df_clean.head()
+category = df['category'].unique()
+
 # Title and headers
 st.title("FITTED AND PREDICTED")
 st.write("**Developers:** Emi Cervantes, Christina Orengo, Nathan Samarasena")
@@ -14,5 +24,6 @@ image = Image.open('Scripts/spongebob.png')
 col1, col2 = st.columns(2)
 with col1:
     st.header("spongebob says 'hello!'")
+    factor = col1.selectbox("Choose Your Cloth Category:", category)
 with col2:
     st.image(image)
