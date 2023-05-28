d11a2c3c506e945cfa4b71e9e39d5cc98d5c5a1d
emicervantes <emicer@hotmail.co.jp>
2023-05-27 18:33:38 -0700

    changed py

--- Scripts/fashion.py
+++ Scripts/fashion.py
@@ -3,9 +3,11 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import altair as alt
+from PIL import Image
 
 # Title and headers
 st.title("FITTED AND PREDICTED")
 st.write("**Developers:** Emi Cervantes, Christina Orengo, Nathan Samarasena")
+image = Image.open('choso.jpeg')
 
 
