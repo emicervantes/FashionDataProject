4a7ee3baffef3613f0820a44b0aa26961b31ef68
emicervantes <emicer@hotmail.co.jp>
2023-05-27 18:55:12 -0700

    added link

--- Scripts/fashion.py
+++ Scripts/fashion.py
@@ -8,6 +8,7 @@ from PIL import Image
 # Title and headers
 st.title("FITTED AND PREDICTED")
 st.write("**Developers:** Emi Cervantes, Christina Orengo, Nathan Samarasena")
+st.write("Check our Github repo: [Click here!](https://github.com/emicervantes/FashionDataProject/tree/main)")
 image = Image.open('Scripts/spongebob.png')
 
 col1, col2 = st.columns(2)
@@ -15,5 +16,3 @@ with col1:
     st.header("spongebob says 'hello!'")
 with col2:
     st.image(image)
-
-st.write("Check our Github repo: ")
