0ca9d24be16e9f26a8b6b239b0834d9cdcea98f3
Emi Cervantes <91242164+emicervantes@users.noreply.github.com>
2023-05-27 21:41:54 -0700

    Update fashion.py

--- Scripts/fashion.py
+++ Scripts/fashion.py
@@ -4,6 +4,7 @@ from PIL import Image
 import pandas as pd
 import numpy as np
 import altair as alt
+import sklearn
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsRegressor
