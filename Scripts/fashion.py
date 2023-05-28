04fcca62b5b65ee625f7d7c94d5022c2042630c2
Emi Cervantes <91242164+emicervantes@users.noreply.github.com>
2023-05-27 21:25:03 -0700

    Update fashion.py

--- Scripts/fashion.py
+++ Scripts/fashion.py
@@ -3,6 +3,7 @@ import streamlit as st
 from PIL import Image
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 import altair as alt
 import sklearn
 from sklearn.linear_model import LinearRegression
