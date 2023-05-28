e9037e1aab972eac50bba79f3d587228449f4cb9
emicervantes <emicer@hotmail.co.jp>
2023-05-27 18:31:57 -0700

    changed py

--- Scripts/fashion.py
+++ Scripts/fashion.py
@@ -6,6 +6,6 @@ import altair as alt
 
 # Title and headers
 st.title("FITTED AND PREDICTED")
-st.write("Developers: Emi Cervantes, Christina Orengo, Nathan Samarasena")
+st.write("**Developers:** Emi Cervantes, Christina Orengo, Nathan Samarasena")
 
 
